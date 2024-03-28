24891cf0e769002c127537660049f9892cf5b71e
hyechol21 <like3orange@gmail.com>
2024-03-28 14:40:17 +0900

    add login

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import authenticate
 
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
@@ -44,3 +45,16 @@ class RegisterSerializer(serializers.ModelSerializer):
         token = Token.objects.create(user=user)
         return user
         
+        
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+    
+    def validate(self, data):
+        user = authenticate(**data)
+        if user:
+            token = Token.objects.get(user=user)
+            return token
+        raise serializers.ValidationError(
+            {"error": "Unable to log in with provided credentials."}
+        )
--- users/views.py
+++ users/views.py
@@ -1,9 +1,20 @@
 from django.contrib.auth.models import User
-from rest_framework import generics
-from .serializers import RegisterSerializer
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import RegisterSerializer, LoginSerializer
 
 
 # Create your views here.
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+    
+    
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        token = serializer.validated_data   # validate()의 리턴값인 Token을 받아옴.
+        return Response({"token": token.key}, status=status.HTTP_200_OK)
