1778dfd0e7305eb93eb3c0fd47d48e8a650a35fe
hyechol21 <like3orange@gmail.com>
2024-03-28 13:17:13 +0900

    add users

---
+++ users/serializers.py
@@ -0,0 +1,46 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from rest_framework.validators import UniqueValidator
+
+
+# 회원가입 시리얼라이저
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required = True,
+        validators = [UniqueValidator(queryset=User.objects.all())],
+    )
+    password = serializers.CharField(
+        write_only = True,
+        required = True,
+        validators = [validate_password],
+    )
+    password2 = serializers.CharField(
+        write_only = True,
+        required = True,
+    )
+    
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password2', 'email')
+        
+    def validate(self, data):   # 비밀번호 일치 여부 확인
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError(
+                {'password': "Password fields didn't match."}
+            )
+        return data
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email = validated_data['email'],
+        )
+        
+        user.set_password(validated_data['password'])
+        user.save()
+        token = Token.objects.create(user=user)
+        return user
+        
