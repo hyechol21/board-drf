47b6273da7c689588d883bf9ed315baa83cbe6b6
hyechol21 <like3orange@gmail.com>
2024-04-02 00:13:41 +0900

    add Post

--- myboard/settings.py
+++ myboard/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'users',
     'corsheaders',
     'posts',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -114,6 +115,9 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
     ],
 }
 
